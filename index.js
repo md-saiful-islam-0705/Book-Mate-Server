900d93db753d01ccdcd46ab499c1036feb733871
Md Saiful Islam <mdsaifulislam@Mds-MacBook-Air.local>
2024-04-27 11:37:45 +0300

    send data for view details single data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -8,7 +9,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gpuomtl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 async function run() {
@@ -48,6 +49,14 @@ async function defineRoutes(client) {
         const result = await spotsCollection.insertOne(newSpot);
         res.send(result);
     })
+    app.get("/spots/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const spot = await spotsCollection.findOne(query);
+        res.json(spot);
+    });
+
+    
     
     
 
