7eaca8c23df38b78a656aac40d9fc0ca11d8a405
Md Saiful Islam <mdsaifulislam@Mds-MacBook-Air.local>
2024-05-04 01:35:36 +0300

    fixed vercel.json and index file for deployment

--- index.js
+++ index.js
@@ -7,158 +7,110 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
+// {origin: "https://journey-junction-2024.web.app/"}
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gpuomtl.mongodb.net`;
+console.log(uri);
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 async function run() {
   try {
-    // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-    const client = new MongoClient(uri, {
-      serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-      },
+      
+
+    const spotsCollection = client.db("spotsDB").collection("spots");
+    const countriesCollection = client.db("spotsDB").collection("countries");
+
+    // Route to fetch all countries
+    app.get("/countries", async (req, res) => {
+      const cursor = countriesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
     });
 
-    // Connect the client to the server
-    await client.connect();
-
-    // Database connection successful, define routes and start server
-    defineRoutes(client);
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-    process.exit(1); // Exit the process if MongoDB connection fails
-  }
-}
-
-async function defineRoutes(client) {
-  const spotsCollection = client.db("spotsDB").collection("spots");
-  const countriesCollection = client.db("spotsDB").collection("countries");
-
-  // Route to fetch all countries
-  app.get("/countries", async (req, res) => {
-    const cursor = countriesCollection.find();
-    const result = await cursor.toArray();
-    res.send(result);
-  });
-
-
-  // Route to fetch all tourist spots
-  app.get("/spots", async (req, res) => {
-    try {
+    // Route to fetch all tourist spots
+    app.get("/spots", async (req, res) => {
       const cursor = spotsCollection.find();
       const result = await cursor.toArray();
       res.send(result);
-    } catch (error) {
-      console.error("Error fetching tourist spots:", error);
-      res.status(500).json({ error: "Failed to fetch tourist spots" });
-    }
-  });
+    });
 
-  // Route to add a new tourist spot
-  app.post("/spots", async (req, res) => {
-    try {
+    // Route to add a new tourist spot
+    app.post("/spots", async (req, res) => {
       const newSpot = req.body;
       const result = await spotsCollection.insertOne(newSpot);
       res.send(result);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error });
-    }
-  });
+    });
 
-  // Route to fetch details of a specific tourist spot
-  app.get("/spots/:id", async (req, res) => {
-    try {
+    // Route to fetch details of a specific tourist spot
+    app.get("/spots/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const spot = await spotsCollection.findOne(query);
       res.json(spot);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error });
-    }
-  });
-  // Route to fetch spots by country
-  // app.get("/spots/:countryName", async (req, res) => {
-  //   try {
-  //     const countryName = req.params.countryName;
-  //     const query = { country_Name: countryName }; // Assuming 'countryName' matches the field name in your database
-  //     const spot = await spotsCollection.findOne(query);
-  //     if (!spot) {
-  //       return res.status(404).json({ error: "Spot not found" });
-  //     }
-  //     res.json(spot);
-  //   } catch (error) {
-  //     console.error(error);
-  //     res.status(500).json({ error: "Failed to fetch spot data" });
-  //   }
-  // });
-  
+    });
 
-  
-  
+    // Route to fetch tourist spots by country
+    // app.get("/spots/:countryName", async (req, res) => {
+    
+    //     const countryName = req.params.countryName;
+    //     const cursor = spotsCollection.find({ country_Name: countryName });
+    //     const result = await cursor.toArray();
+    //     res.send(result);
+    // });
 
-  // Route to fetch tourist spots by country
-// app.get("/spots/:countryName", async (req, res) => {
-//   try {
-//     const countryName = req.params.countryName;
-//     const cursor = spotsCollection.find({ country_Name: countryName });
-//     const result = await cursor.toArray();
-//     res.send(result);
-//   } catch (error) {
-//     console.error("Error fetching tourist spots:", error);
-//     res.status(500).json({ error: "Failed to fetch tourist spots" });
-//   }
-// });
-
-
-  // Route to fetch user's tourist spots
-  app.get("/user-spots", async (req, res) => {
-    const userEmail = req.query.userEmail;
-    try {
+    // Route to fetch user's tourist spots
+    app.get("/user-spots", async (req, res) => {
+      const userEmail = req.query.userEmail;
       const userSpots = await spotsCollection.find({ userEmail }).toArray();
       res.json(userSpots);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error });
-    }
-  });
-  // Delete
-  app.delete("/user-spots/:id", async (req, res) => {
-    const spotId = req.params.id;
-    const query = { _id: new ObjectId(spotId) };
-    const result = await spotsCollection.deleteOne(query);
-    res.send(result);
-  });
-
-  // route for updating a user spot
-  app.put("/user-spots/:id", async (req, res) => {
-    const spotId = req.params.id;
-    const updatedSpot = req.body;
-
-    const query = { _id: new ObjectId(spotId) };
-    const updateResult = await spotsCollection.updateOne(query, {
-      $set: updatedSpot,
     });
-    if (updateResult.matchedCount === 0) {
-      return res.status(404).json({ error: "User spot not found" });
-    }
-    res.json({ message: "User spot updated successfully" });
-  });
+    // Delete
+    app.delete("/user-spots/:id", async (req, res) => {
+      const spotId = req.params.id;
+      const query = { _id: new ObjectId(spotId) };
+      const result = await spotsCollection.deleteOne(query);
+      res.send(result);
+    });
 
-  // Send a ping to confirm a successful connection
-  await client.db("admin").command({ ping: 1 });
-  console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // route for updating a user spot
+    app.put("/user-spots/:id", async (req, res) => {
+      const spotId = req.params.id;
+      const updatedSpot = req.body;
 
-  // Start the server
-  app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-  });
+      const query = { _id: new ObjectId(spotId) };
+      const updateResult = await spotsCollection.updateOne(query, {
+        $set: updatedSpot,
+      });
+      if (updateResult.matchedCount === 0) {
+        return res.status(404).json({ error: "User spot not found" });
+      }
+      res.json({ message: "User spot updated successfully" });
+    });
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
 }
+run().catch(console.dir);
 
-// Run the server
-run().catch(console.error);
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
+app.get('/', (req, res) => {
+  res.send(' server is running')
+})
